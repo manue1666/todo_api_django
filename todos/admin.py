ad07bec9ec90445e19b7b878ecd506ca2b6bfd8a
manue1666 <utm23090708@utma.edu.mx>
2026-02-22 23:38:26 -0600

    feature(todos): models creation and migration

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Task
+from .models import User
 
 # Register your models here.
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("user", "title", "completed", "created_at")
+    list_filter = ("completed", "created_at")
+    search_fields = ("title", "description")
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "created_at")
+    search_fields = ("name", "email")
--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    deleted = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE, related_name="tasks")
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=500, null=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    deleted = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+
+    def __str__(self):
+        return self.title
+    
